59eb185a40ac25fa6a8c79c494da21d5c9308cce
Milt Reder <milt@yetanalytics.com>
2018-04-06 18:52:27 -0400

    added in-memory LRS implementation stub

--- project.clj
+++ project.clj
@@ -6,4 +6,14 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.yetanalytics/xapi-schema "1.0.0-alpha10-SNAPSHOT"]]
   :profiles {:dev
-             {:dependencies [[org.clojure/test.check "0.10.0-alpha2"]]}})
+             {:source-paths ["src" "dev"]
+              :repl-options {:init-ns user}
+              :dependencies [[org.clojure/test.check "0.10.0-alpha2"]
+                             [io.pedestal/pedestal.service "0.5.3"]
+                             [io.pedestal/pedestal.jetty "0.5.3"]
+                             [ch.qos.logback/logback-classic "1.1.8" :exclusions [org.slf4j/slf4j-api]]
+                             [org.slf4j/jul-to-slf4j "1.7.22"]
+                             [org.slf4j/jcl-over-slf4j "1.7.22"]
+                             [org.slf4j/log4j-over-slf4j "1.7.22"]
+                             [io.pedestal/pedestal.service-tools "0.5.3"]
+                             ]}})
