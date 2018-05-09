6beac5fa7ebe7d592a27c61b67f4c7a8711c6cb2
Milt Reder <milt@yetanalytics.com>
2018-05-09 14:55:50 -0400

    document route improvements, cool kid nio GET response

--- src/com/yetanalytics/lrs/pedestal/routes/documents.clj
+++ src/com/yetanalytics/lrs/pedestal/routes/documents.clj
@@ -4,7 +4,8 @@
             [cheshire.core :as json]
             [clojure.java.io :as io])
   (:import [com.google.common.io ByteStreams]
-           [java.io InputStream]))
+           [java.io InputStream ByteArrayOutputStream]
+           [java.nio ByteBuffer]))
 ;; TODO: Handle io correctly
 
 (def handle-put
@@ -29,13 +30,19 @@
                        com.yetanalytics/lrs] :as ctx}]
             (let [{[doc-type params] ::p/set-document-params} xapi
                   {:keys [body content-type content-length]} request]
-              (lrs/set-document
-               lrs params
-               {:content-type content-type
-                :content-length content-length
-                :contents (ByteStreams/toByteArray ^InputStream body)}
-               true)
-              (assoc ctx :response {:status 204})))})
+              (try (lrs/set-document
+                    lrs params
+                    {:content-type content-type
+                     :content-length content-length
+                     :contents (ByteStreams/toByteArray ^InputStream body)}
+                    true)
+                   (assoc ctx :response {:status 204})
+                   (catch clojure.lang.ExceptionInfo exi
+                     (let [exd (ex-data exi)]
+                       (case (:type exd)
+                         :com.yetanalytics.lrs.xapi.document/invalid-merge
+                         (assoc ctx :response {:status 400})
+                         (throw exi)))))))})
 
 (def handle-get
   {:name ::handle-get
@@ -45,25 +52,36 @@
             (let [{[card [doc-type params]]
                    ::p/get-document-all-params} xapi
                   ]
-              (if-let [result
-                       (case card
-                         :single
-                         (lrs/get-document lrs params)
-                         :multiple
-                         (lrs/get-document-ids lrs params))]
-                (assoc ctx :response {:status 200
-                                      :body result})
-                (assoc ctx :response {:status 404}))))})
+              (case card
+                :single
+                (if-let [{:keys [content-type
+                                 content-length
+                                 contents
+                                 id
+                                 updated] :as result} (lrs/get-document lrs params)]
+                  (assoc ctx :response {:status 200
+                                        :headers {"Content-Type" content-type
+                                                  "Content-Length" (str content-length)
+                                                  "Last-Modified" updated}
+                                        :body (ByteBuffer/wrap ^bytes contents) #_(ByteArrayOutputStream. ^bytes contents)
+                                        #_(if (.startsWith ^String content-type "application/json")
+                                            (json/generate-string contents)
+                                            (String. ^bytes contents "UTF-8"))})
+                  (assoc ctx :response {:status 404}))
+                :multiple
+                (if-let [result (lrs/get-document-ids lrs params)]
+                  (assoc ctx :response {:status 200
+                                        :body result})
+                  (assoc ctx :response {:status 200
+                                        :body []})))))})
 
 (def handle-delete
   {:name ::handle-delete
    :enter (fn [{:keys [xapi
                        request
                        com.yetanalytics/lrs] :as ctx}]
-            (let [{{[card [doc-type params]]
-                    ::p/delete-document-all-params}
-                   ::p/delete-document-any-params} xapi
-                  ]
+            (let [{[card [doc-type params]]
+                   ::p/delete-document-all-params} xapi]
               (case card
                 :single (lrs/delete-document lrs params)
                 :multiple (lrs/delete-documents lrs params))
