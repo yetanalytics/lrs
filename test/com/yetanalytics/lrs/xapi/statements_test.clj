41f82ca5e3b29a82a91051c29d24a216af032c61
Milt Reder <milt@yetanalytics.com>
2018-05-11 14:52:11 -0400

    migrated other test namespaces to use the new macros

--- test/com/yetanalytics/lrs/xapi/statements_test.clj
+++ test/com/yetanalytics/lrs/xapi/statements_test.clj
@@ -8,19 +8,3 @@
 
 (deftest-check-ns ns-test com.yetanalytics.lrs.xapi.statements
   {:default {::stc/opts {:num-tests 10 :max-size 10}}})
-
-
-#_(deftest statements-test
-  (let [results (stest/check
-                 (stest/enumerate-namespace 'com.yetanalytics.lrs.xapi.statements)
-                 {::stc/opts {:num-tests 10 :max-size 10}})
-        failures
-        (keep (fn [result]
-                (when (false? (:failure result))
-                  (-> result
-                      (update ::stc/ret dissoc
-                              :result-data
-                              :fail)
-                      (dissoc :spec))))
-              results)]
-    (is (empty? failures))))
