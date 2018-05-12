c2e18d099127cd70a4fa75cd9e656b10fa1f15ed
Milt Reder <milt@yetanalytics.com>
2018-05-12 13:40:05 -0400

    generic sha-1 hash, but not sure if we'll even need it

---
+++ src/com/yetanalytics/lrs/util/hash.clj
@@ -0,0 +1,37 @@
+(ns com.yetanalytics.lrs.util.hash
+  (:require [clojure.java.io :as io])
+  (:import [java.security MessageDigest]
+           [java.io File]
+           [java.nio ByteBuffer]
+           [java.nio.file Files]
+           ))
+
+(set! *warn-on-reflection* true)
+
+(defn bytes-sha-1
+  "Core sha-1 implementation"
+  ^String [^bytes bs]
+  (apply str
+         (map
+          #(.substring
+            (Integer/toString
+             (+ (bit-and % 0xff) 0x100) 16) 1)
+          (.digest (MessageDigest/getInstance "SHA-1")
+                   bs))))
+
+(defmulti sha-1 class)
+
+(defmethod sha-1 (Class/forName "[B") ^String [^bytes bs]
+  (bytes-sha-1 bs))
+
+(defmethod sha-1 String
+  ^String [^String s]
+  (bytes-sha-1 (.getBytes s "UTF-8")))
+
+(defmethod sha-1 ByteBuffer
+  ^String [^ByteBuffer bb]
+  (bytes-sha-1 (.array bb)))
+
+(defmethod sha-1 :default
+  [x]
+  (bytes-sha-1 (byte-array [(hash x)])))
