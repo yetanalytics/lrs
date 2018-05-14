a3bc9a0ba8429a445ce04868815c53953371f40f
Milt Reder <milt@yetanalytics.com>
2018-05-13 20:01:44 -0400

    added some spec utils for async stuff

--- src/com/yetanalytics/lrs/spec/common.clj
+++ src/com/yetanalytics/lrs/spec/common.clj
@@ -1,6 +1,8 @@
 (ns com.yetanalytics.lrs.spec.common
   (:require [clojure.spec.alpha :as s]
-            [clojure.spec.gen.alpha :as sgen]))
+            [clojure.spec.gen.alpha :as sgen]
+            [clojure.core.async :as a]
+            [clojure.core.async.impl.protocols :as ap]))
 
 (def string-ascii-not-empty
   (s/with-gen (s/and string?
@@ -23,3 +25,29 @@
     (fn []
       (sgen/fmap (partial s/conform spec)
                  (s/gen spec)))))
+
+;; Async Spec Helpers
+
+(defn read-port?
+  "Predicate to see if something is a read-port?"
+  [c]
+  (satisfies? ap/ReadPort c))
+
+(defn fake-chan
+  "Makes a fake channel holding x"
+  [x]
+  (reify
+    ap/ReadPort
+    (take! [_ _]
+      (delay x))))
+
+(defmacro from-port [spec]
+  `(s/with-gen
+     (s/and
+      read-port?
+      (s/conformer a/<!!)
+      ~spec)
+     (fn []
+       (sgen/fmap
+        fake-chan
+        (s/gen ~spec)))))
