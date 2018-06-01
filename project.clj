e502e9a233d2b674807106f6f2f64236a1bc2fb0
Milt Reder <milt@yetanalytics.com>
2018-06-01 10:33:15 -0400

    but we do need google guava I guess

--- project.clj
+++ project.clj
@@ -6,7 +6,9 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.yetanalytics/xapi-schema "1.0.0-alpha17-SNAPSHOT"
                   :exclusions [org.clojure/clojurescript]]
-                 [io.pedestal/pedestal.service "0.5.3"]]
+                 [io.pedestal/pedestal.service "0.5.3"]
+                 ;; TODO: Find a replacement where we use guava
+                 [com.google.guava/guava "25.1-jre"]]
   :profiles {:dev
              {:source-paths ["src" "dev"]
               :clean-targets ^{:protect false} ["lrs-conformance-test-suite" "node_modules"]
