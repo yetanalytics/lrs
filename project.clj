984c66539a9d3eeb22719d65258bafa94f86a55b
Milt Reder <milt@yetanalytics.com>
2018-06-01 10:27:58 -0400

    removed some duplicated + unneeded deps

--- project.clj
+++ project.clj
@@ -4,16 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.yetanalytics/xapi-schema "1.0.0-alpha17-SNAPSHOT"]
-                 [io.pedestal/pedestal.service "0.5.3"]
-                 [buddy/buddy-sign "2.2.0"]]
+                 [com.yetanalytics/xapi-schema "1.0.0-alpha17-SNAPSHOT"
+                  :exclusions [org.clojure/clojurescript]]
+                 [io.pedestal/pedestal.service "0.5.3"]]
   :profiles {:dev
              {:source-paths ["src" "dev"]
               :clean-targets ^{:protect false} ["lrs-conformance-test-suite" "node_modules"]
               :main mem-lrs.server
               :repl-options {:init-ns user}
               :dependencies [[org.clojure/test.check "0.10.0-alpha2"]
-                             [io.pedestal/pedestal.service "0.5.3"]
                              [io.pedestal/pedestal.jetty "0.5.3"]
                              [ch.qos.logback/logback-classic "1.1.8" :exclusions [org.slf4j/slf4j-api]]
                              [org.slf4j/jul-to-slf4j "1.7.22"]
