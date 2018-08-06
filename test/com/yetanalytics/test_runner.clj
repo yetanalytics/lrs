8e5cb88c8f11a5360b909414045bb1d2da0c1742
Milt Reder <milt@yetanalytics.com>
2018-08-06 15:42:06 -0400

    remove bad dummy test

--- test/com/yetanalytics/test_runner.clj
+++ test/com/yetanalytics/test_runner.clj
@@ -7,12 +7,9 @@
             com.yetanalytics.lrs.xapi.statements-test
             ))
 
-(deftest bad-test (is false))
-
 (defn -main []
   (let [{:keys [test pass fail error] :as result}
         (run-tests
-         'com.yetanalytics.test-runner
          'com.yetanalytics.lrs-test
          'com.yetanalytics.lrs.xapi.activities-test
          'com.yetanalytics.lrs.xapi.agents-test
