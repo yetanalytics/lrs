8a01a298b5b2b47a2c12338d2484352948b029fd
Milton Reder <milt@yetanalytics.com>
2021-09-02 15:24:04 -0400

    Lrs 49 improve ci (#51)
    * try matrix-based ci
    
    * Attempt completely parallel ci

---
+++ src/test/com/yetanalytics/conformance_test/clj/sync.clj
@@ -0,0 +1,20 @@
+(ns com.yetanalytics.conformance-test.clj.sync
+  (:require
+   [clojure.test :refer :all]
+   [com.yetanalytics.lrs.test-runner :as runner]
+   [mem-lrs.server :as server]
+   [io.pedestal.http :as http]))
+
+(use-fixtures :once runner/test-suite-fixture)
+
+(deftest test-lrs-sync
+  (testing "clj/java sync"
+    (let [s (server/run-dev :reload-routes? false
+                            :mode :sync)
+          ret (runner/conformant?
+               "-e"
+               "http://localhost:8080/xapi"
+               "-b"
+               "-z")]
+      (http/stop s)
+      (is (true? ret)))))
